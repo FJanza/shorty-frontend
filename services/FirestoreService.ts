ca89f8d3bba87c7c75a4a414060b2712a48a0323
fjanza <franciscojanza001@gmail.com>
2025-01-30 22:38:09 -0300

    fix reciveUserInfo bug

--- services/FirestoreService.ts
+++ services/FirestoreService.ts
@@ -15,6 +15,8 @@ interface AddNewUserProps {
 }
 
 export const addNewUser = async ({uid, email}: AddNewUserProps) => {
+  let userExists = false;
+
   const querySnapshot = await getDocs(collection(db, "users"));
   querySnapshot.forEach((doc) => {
     if (doc.data().email === email && doc.id !== uid) {
@@ -23,16 +25,43 @@ export const addNewUser = async ({uid, email}: AddNewUserProps) => {
         status: "error",
         message: "User already exists",
       };
+    } else {
+      userExists = true;
     }
   });
 
-  await setDoc(doc(db, "users", uid), {
-    email: email,
-    receiveUserInfo: false,
-  });
+  if (!userExists) {
+    await setDoc(doc(db, "users", uid), {
+      email: email,
+      receiveUserInfo: false,
+    });
+  }
 
   return {
     status: "ok",
     message: "User added successfully",
+  };
+};
+
+export const getUserReceiveInfo = async (email: string) => {
+  const querySnapshot = await getDocs(collection(db, "users"));
+  let receiveUserInfo = null;
+
+  querySnapshot.forEach((doc) => {
+    if (doc.data().email === email) {
+      receiveUserInfo = doc.data().receiveUserInfo;
+    }
+  });
+
+  if (receiveUserInfo === null) {
+    return {
+      status: "error",
+      message: "User not found",
+    };
+  }
+
+  return {
+    status: "ok",
+    receiveUserInfo: receiveUserInfo,
   };
 };
